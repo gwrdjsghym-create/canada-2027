ad02f9e27cabf1066c86717f2b99b2194ca8756d
gwrdjsghym-create <gwrdjsghym@privaterelay.appleid.com>
2026-09-08 18:33:25 +0200

    Center traveller poster lightbox

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $profile = CANADA_PROFILES[$profileId];
     <meta name="description" content="Gemeinsame Reiseplanung für Canada 2027" />
     <meta name="robots" content="noindex,nofollow" />
     <title>Canada 2027</title>
-    <link rel="stylesheet" href="styles.css?v=20260908-6" />
+    <link rel="stylesheet" href="styles.css?v=20260908-7" />
   </head>
   <body>
     <header class="topbar">
