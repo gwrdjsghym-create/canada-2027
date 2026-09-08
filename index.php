0bb466f2bb908fac2596b761fff09acd65af413c
gwrdjsghym-create <gwrdjsghym@privaterelay.appleid.com>
2026-09-08 21:13:29 +0200

    Refresh tutorial script cache

--- index.php
+++ index.php
@@ -112,6 +112,6 @@ $profile = CANADA_PROFILES[$profileId];
         </footer>
       </article>
     </dialog>
-    <script src="app.js?v=20260908-4"></script>
+    <script src="app.js?v=20260908-5"></script>
   </body>
 </html>
