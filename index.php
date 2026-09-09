f8944aa849cd1ab515ff7bdac959eb2dd2b8c814
gwrdjsghym-create <gwrdjsghym@privaterelay.appleid.com>
2026-09-09 21:00:17 +0200

    Aktualisiere App-Cache nach Übersichtsbereinigung

--- index.php
+++ index.php
@@ -112,6 +112,6 @@ $profile = CANADA_PROFILES[$profileId];
         </footer>
       </article>
     </dialog>
-    <script src="app.js?v=20260909-1"></script>
+    <script src="app.js?v=20260909-7"></script>
   </body>
 </html>
